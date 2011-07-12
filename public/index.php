246962890b9adde500a41448b9b8c47c8baaf2d3
Andreas Hucks <andreas.hucks@duochrome.net>
2011-07-12 16:51:28 +0200

    initial setup

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../silex.phar';
+
+$app = new Silex\Application();
+
+$app->get('/hello/{name}', function ($name) use ($app)
+{
+    return 'Hello ' . $app->escape($name);
+});
+
+$app->run();
