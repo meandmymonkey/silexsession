a35ae429cf3ea1afe0c4403ed8b236e225d75dab
Andreas Hucks <andreas.hucks@duochrome.net>
2011-07-13 11:49:33 +0200

    added section comments

--- public/index.php
+++ public/index.php
@@ -2,11 +2,23 @@
 
 require_once __DIR__ . '/../silex.phar';
 
+/**
+* Setup
+*/
+
 $app = new Silex\Application();
+
+/**
+* Controller setup
+*/
 
 $app->get('/hello/{name}', function ($name) use ($app)
 {
     return 'Hello ' . $app->escape($name);
 });
+
+/**
+* Run application
+*/
 
 $app->run();
