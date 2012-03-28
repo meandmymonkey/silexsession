91fcc91076aceae69209f23f0a41cc8f5a3b54d8
Andreas Hucks <andreas.hucks@sensiolabs.de>
2012-03-28 05:41:32 -0700

    updated request injection

--- public/postRequest.php
+++ public/postRequest.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../silex.phar';
 use Silex\ControllerCollection;
 use SilexWorkshop\Model\Comment;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
 * Setup
@@ -16,10 +17,8 @@ $postApp = new ControllerCollection();
 * Controller setup
 */
 
-$postApp->post('/new', function () use ($postApp)
+$postApp->post('/new', function (Request $request) use ($postApp)
 {
-    $request =  $postApp['request'];
-
     $comment = new Comment();
     $comment->content = $request->get('comment');
 
